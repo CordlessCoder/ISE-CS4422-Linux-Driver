b3e639d1146c2be1a60a865e914e56c30bf74687
CordlessCoder <roman@vm.net.ua>
2026-03-18 00:15:28 +0000

    Slightly more thorough testing

--- chatest/src/main.rs
+++ chatest/src/main.rs
@@ -12,44 +12,45 @@ use chacha20::{
 };
 use nix::{ioctl_none, ioctl_write_int, ioctl_write_ptr};
 
-const DATA: [u8; 6] = *b"foobar";
+const DATA: &str = "This is just an example message, repeated a couple of times
+This is just an example message, repeated a couple of times
+This is just an example message, repeated a couple of times
+";
 const KEY: [u8; 32] = *b"I am a 32-byte key, on god fr fr";
 const NONCE: [u8; 8] = *b"12345678";
 
-// #define SET_KEY _IOW('s', 'k', char[32])
-// #define SET_NONCE _IOW('s', 'n', char[8])
-// #define RESET_COUNTER _IO('r', 'c')
-// #define SET_COUNTER _IOW('s', 'c', int*)
 ioctl_write_ptr!(cha_set_key, 's', 'k', [u8; 32]);
 ioctl_write_ptr!(cha_set_nonce, 's', 'n', [u8; 8]);
 ioctl_none!(cha_reset_counter, 'r', 'c');
 ioctl_write_int!(cha_set_counter, 's', 'c');
 
-fn main() {
+fn test_message(message: &str) {
+    println!("Testing message {message}");
     let mut chacha = File::options()
         .write(true)
         .read(true)
         .open("/dev/chacha")
         .unwrap();
     unsafe {
-        let mut key = KEY;
-        let mut nonce = NONCE;
-        cha_set_key(chacha.as_raw_fd(), &key).unwrap();
-        cha_set_nonce(chacha.as_raw_fd(), &nonce).unwrap();
+        cha_set_key(chacha.as_raw_fd(), &KEY).unwrap();
+        cha_set_nonce(chacha.as_raw_fd(), &NONCE).unwrap();
     }
     let mut reference = chacha20::ChaCha20Legacy::new(&Array(KEY), &Array(NONCE));
 
-    let mut ref_buf = DATA;
-    let mut buf = DATA;
+    let mut ref_buf = message.as_bytes().to_vec();
+    let mut buf = ref_buf.clone();
     println!("Original: {}", Path::new(OsStr::from_bytes(&buf)).display());
 
     chacha.write_all(&buf).unwrap();
-    chacha.read_exact(&mut buf).unwrap();
+    let third = buf.len() / 3;
+    chacha.read_exact(&mut buf[..third]).unwrap();
+    chacha.read_exact(&mut buf[third..]).unwrap();
 
     reference.apply_keystream(&mut ref_buf);
 
     println!("Encrypted: {buf:?}",);
     println!("Encrypted(ref): {ref_buf:?}",);
+    assert_eq!(buf, ref_buf);
 
     unsafe {
         cha_reset_counter(chacha.as_raw_fd()).unwrap();
@@ -57,7 +58,8 @@ fn main() {
 
     reference.seek(0);
     chacha.write_all(&buf).unwrap();
-    chacha.read_exact(&mut buf).unwrap();
+    chacha.read_exact(&mut buf[..third]).unwrap();
+    chacha.read_exact(&mut buf[third..]).unwrap();
     reference.apply_keystream(&mut ref_buf);
     println!(
         "Decrypted: {}",
@@ -67,4 +69,11 @@ fn main() {
         "Decrypted(ref): {}",
         Path::new(OsStr::from_bytes(&ref_buf)).display()
     );
+    assert_eq!(buf, ref_buf);
+}
+
+fn main() {
+    test_message(&DATA[..20]);
+    test_message(&DATA[..119]);
+    test_message(DATA);
 }
