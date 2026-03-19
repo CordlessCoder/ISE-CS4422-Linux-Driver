0a04ca725e5079e375b97b739056eb256afa9338
CordlessCoder <roman@vm.net.ua>
2026-03-19 21:24:36 +0000

    Tiny formatting fix

--- driver/chacha_ioctl.c
+++ driver/chacha_ioctl.c
@@ -3,7 +3,7 @@
 
 long int chacha_ioctl(struct file* f, unsigned int cmd, unsigned long args) {
     atomic64_inc(&lchacha_stats.ioctls);
-    
+
     chacha_state* state = f->private_data;
     dev_dbg(lchacha_dev, "ioctl called with cmd: 0x%x and args: %p\n", cmd, (void*)args);
     int status = 0;
