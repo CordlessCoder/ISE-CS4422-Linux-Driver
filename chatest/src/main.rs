d79077bb250e659d14a3d896f355f895747623f4
CordlessCoder <roman@vm.net.ua>
2026-03-19 14:49:04 +0000

    Improved throughput test

--- chatest/src/main.rs
+++ chatest/src/main.rs
@@ -1,15 +1,10 @@
 use std::{
-    ffi::OsStr,
     fs::File,
-    io::{Read, Seek, SeekFrom, Write},
-    os::{fd::AsRawFd, unix::ffi::OsStrExt},
-    path::Path,
+    io::Read,
+    os::fd::AsRawFd,
     sync::atomic::AtomicBool,
-    thread,
 };
-
-use nix::{ioctl_none, ioctl_write_int, ioctl_write_ptr};
-use rand::Rng;
+use nix::{ioctl_none, ioctl_write_ptr};
 
 const KEY: [u8; 32] = *b"I am a 32-byte key, on god fr fr";
 const NONCE: [u8; 8] = *b"12345678";
@@ -18,7 +13,9 @@ const BUF_SIZE: usize = 3984;
 ioctl_write_ptr!(cha_set_key, 's', 'k', [u8; 32]);
 ioctl_write_ptr!(cha_set_nonce, 's', 'n', [u8; 8]);
 ioctl_none!(cha_reset_counter, 'r', 'c');
-ioctl_write_int!(cha_set_counter, 's', 'c');
+ioctl_write_ptr!(cha_set_counter, 's', 'c', u64);
+ioctl_none!(cha_clear_output_only, 'c', 'o');
+ioctl_none!(cha_set_output_only, 's', 'o');
 
 static STOP_FLAG: AtomicBool = AtomicBool::new(false);
 
@@ -37,19 +34,11 @@ fn main() {
     unsafe {
         cha_set_key(chacha.as_raw_fd(), &KEY).unwrap();
         cha_set_nonce(chacha.as_raw_fd(), &NONCE).unwrap();
+        cha_set_output_only(chacha.as_raw_fd()).unwrap();
     }
     let mut chacha = &chacha;
-    thread::scope(|s| {
-        let mut data = vec![0; BUF_SIZE * 1024];
-        let mut output = data.clone();
-        rand::rng().fill_bytes(&mut data);
-        s.spawn(move || {
-            loop {
-                chacha.write_all(&data).unwrap();
-            }
-        });
-        while !STOP_FLAG.load(std::sync::atomic::Ordering::Relaxed) {
-            _ = chacha.read(&mut output).unwrap();
-        }
-    });
+    let mut buf = vec![0; BUF_SIZE * 1024];
+    while !STOP_FLAG.load(std::sync::atomic::Ordering::Relaxed) {
+        _ = chacha.read(&mut buf).unwrap();
+    }
 }
